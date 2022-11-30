176389cb3e6bd9e71c22f52ab5dff33cd15e70be
Grant Fowler <gcf.rxbots@gmail.com>
2022-11-30 15:52:11 -0500

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,88 @@
+import os
+import sys
+import speech_recognition as sr
+import pydub
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+import csv
+
+# pip install SpeechRecognition pydub
+# pip install ffmpeg moviepy
+
+
+r = sr.Recognizer()
+
+def get_large_audio_transcription(path):
+    """
+    Splitting the large audio file into chunks
+    and apply speech recognition on each of these chunks
+    """
+    # open the audio file using pydub
+    sound = AudioSegment.from_file(path, format="mp4")
+    # split audio sound where silence is 700 miliseconds or more and get chunks
+    chunks = split_on_silence(sound,
+        # experiment with this value for your target audio file
+        min_silence_len = 500,
+        # adjust this per requirement
+        silence_thresh = sound.dBFS-14,
+        # keep the silence for 1 second, adjustable as well
+        keep_silence=500,
+    )
+    folder_name = "audio-chunks"
+    # create a directory to store the audio chunks
+    if not os.path.isdir(folder_name):
+        os.mkdir(folder_name)
+    whole_text = ""
+    # process each chunk
+    for i, audio_chunk in enumerate(chunks, start=1):
+        # export audio chunk and save it in
+        # the `folder_name` directory.
+        chunk_filename = os.path.join(folder_name, f"chunk{i}.wav")
+        audio_chunk.export(chunk_filename, format="wav")
+        # recognize the chunk
+        with sr.AudioFile(chunk_filename) as source:
+            audio_listened = r.record(source)
+            # try converting it to text
+            try:
+                text = r.recognize_google(audio_listened)
+            except sr.UnknownValueError as e:
+                print("Error:", str(e))
+            else:
+                text = f"{text.capitalize()}. "
+                #print(text)
+                whole_text += text
+    # return the text for all chunks detected
+    return whole_text
+
+
+
+if __name__ == "__main__":
+    with open("hotwords.txt", "r") as f:
+        hotwords = f.read().replace(" ", "")
+        hotwords = hotwords.split(",")
+        print(hotwords)
+
+
+    filesInVideosFolder = os.listdir("videos")
+    with open("output.csv", "w", newline='') as csvFile:
+        csvwriter = csv.writer(csvFile)
+        for video in filesInVideosFolder:
+            toWrite = [video, "NO HOTWORD DETECTED"]
+            print("Getting video text for %s" % video)
+            videoText = get_large_audio_transcription("videos/%s" % video)
+
+            for hotword in hotwords:
+                if hotword in videoText:
+                    sentenceLocation = videoText.split(" ").index(hotword)
+                    sentence = (" ".join((videoText.split(" ")[abs(sentenceLocation - 6):abs(sentenceLocation + 7)])))
+                    if "NO HOTWORD DETECTED" in toWrite:
+                        toWrite.remove("NO HOTWORD DETECTED")
+                    toWrite.append("HOTWORD: " + hotword)
+                    toWrite.append("SENTENCE: " + sentence)
+
+            print(toWrite)
+            csvwriter.writerow(toWrite)
+
+
+
+
